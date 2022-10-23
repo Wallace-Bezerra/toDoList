2b121cef6c4d8820cdb5c8c890d25b4399b8c95f
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-10-22 22:30:57 -0300

    localStorage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const Main = {
+  
   init: function () {
     this.cacheSelectors();
     this.bindEvents();
+    
+    // console.log(this.tarefasObj)
+    
   },
   cacheSelectors: function () {
     this.$checkButtons = document.querySelectorAll(".check");
@@ -21,21 +25,73 @@ const Main = {
       item.addEventListener('click', this.Events.deleteTask);
     })
   },
+  
+  getStoraged:()=>{
+    
+    const newTarefas = JSON.parse(localStorage.getItem("tarefas"))
+    if(newTarefas){
+      Main.tasks = newTarefas;
+    }
+    console.log("tarefas",Main.tasks)
+  },
+  tasks:[],
+  buildTasks:()=>{
+    Main.tasks.forEach((item)=>{
+      let classe;
+      if(item.done){
+        classe = 'class="done"';
+      }
+      else{
+        classe = 'class=""';
+      }
+      document.querySelector(".list").innerHTML += `<li ${classe}>
+      <div class="check"></div>
+      <label class="task">
+        ${item.tarefa}
+      </label>
+      <button class="remove"></button>
+    </li>`
+    })
+    
+  },
+
   Events: {
     checkButton_click: (event) => {
-      event.currentTarget.parentElement.classList.toggle("done");
-      // event.currentTarget.classList.add("teste");
+      const li = event.currentTarget.parentElement;
+      const task = li.querySelector(".task");
+
+      Main.tasks.forEach((item)=>{
+        if(item.tarefa === task.innerText){
+          if(li.classList.contains("done")){
+            item.done = false;
+          }
+          else{
+            item.done = true;
+          }
+        }
+      });
+      localStorage.setItem('tarefas', JSON.stringify(Main.tasks));
+      console.log(Main.tasks);
+
+      if(li.classList.contains("done")){
+        li.classList.remove("done");
+      }
+      else{
+        li.classList.add("done");
+      }
+      
+      
     },
     inputEnter: (event) => {
 
       if (event.key === "Enter" && event.currentTarget.value != "") {
-        console.log(event.currentTarget);
+        // console.log(event.currentTarget);
         const valor = event.currentTarget.value
+
+        Main.tasks.push( {tarefa: valor,done:false});
+        localStorage.setItem('tarefas', JSON.stringify(Main.tasks));
+
         event.currentTarget.value = "";
-        // const newTask = Main.$checkButtons[0].parentElement.cloneNode(true);
-        // newTask.querySelector(".task").innerText = valor;
-        // Main.$list.appendChild(newTask);
-        // console.log(this);
         Main.$list.innerHTML += `<li>
         <div class="check"></div>
         <label class="task">
@@ -44,20 +100,35 @@ const Main = {
         <button class="remove"></button>
       </li>`
         Main.init();
-        // console.log(newTask);
       }
     },
     deleteTask: (event) => {
       const li = event.currentTarget.parentElement;
+      const task = li.querySelector(".task");
+      Main.tasks.forEach((item,index,array)=>{
+      if(item.tarefa === task.innerText){
+        array.splice(index,1);
+        console.log("deletado",item)
+      }
+      });
+      // const newArray = Main.tasks.filter((item)=>{
+      //   return item.tarefa !== task.innerText
+      // })
+      localStorage.setItem('tarefas', JSON.stringify(Main.tasks));
+      
       li.classList.add("removed");
-
-      // event.currentTarget.parentElement.remove();
       setTimeout(() => {
         li.classList.add("hidden");
+        li.remove();
       }, 300);
+     
 
-      // event.currentTarget.parentElement.remove();
     }
   }
 }
+
+Main.getStoraged();
+Main.buildTasks();
 Main.init();
+
+
